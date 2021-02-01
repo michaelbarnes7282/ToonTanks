8005c8ca352717f322f57b055fa778af49f51028
Michael Barnes <michaelbarnes7282@gmail.com>
2021-02-01 14:45:02 -0800

    Added functionality to allow turrets to calculate distance from player

--- Source/ToonTanks/Pawns/PawnTurret.cpp
+++ Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -3,26 +3,46 @@
 
 #include "PawnTurret.h"
 #include "Kismet/GameplayStatics.h"
+#include "PawnTank.h"
 
 // Called when the game starts or when spawned
 void APawnTurret::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
     GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+
+    PlayerPawn = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
 }
 
 // Called every frame
 void APawnTurret::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 }
 
 void APawnTurret::CheckFireCondition() 
 {
     // If Player == null || is Dead THEN BAIL!!
+    if(!PlayerPawn)
+    {
+        return;
+    }
+    // IF Player IS in range THEN FIRE!! 
+    if(ReturnDistanceToPlayer() <= FireRange)
+    {
+        // Fire
+        UE_LOG(LogTemp, Warning, TEXT("Fire Condition Checked"));
+    }
+   
+}
 
-    // If Player IS in range THEN FIRE!!
+float APawnTurret::ReturnDistanceToPlayer() 
+{
+    if (!PlayerPawn)
+    {
+        0.0f;
+    }
 
-    UE_LOG(LogTemp, Warning, TEXT("Fire Condition Checked!"));
+    return FVector::Dist(PlayerPawn->GetActorLocation(), GetActorLocation());
 }
